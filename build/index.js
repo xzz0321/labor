37ace0a1a2d6950d5d33641035dae471c94297db
杨贤贤 <13671115805@163.com>
2020-07-20 19:01:06 +0800

    修改配置

--- build/index.js
+++ build/index.js
@@ -24,11 +24,9 @@ if (process.env.npm_config_preview || rawArgv.includes('--preview')) {
   )
 
   app.listen(port, function () {
-    // console.log(chalk.green(`> Preview at  http://localhost:${port}${publicPath}`))
-    console.log(chalk.green(`> Preview at  http://192.168.0.119:8080${publicPath}`))
+    console.log(chalk.green(`> Preview at  http://localhost:${port}${publicPath}`))
     if (report) {
-      console.log(chalk.green(`> Report at  http://192.168.0.119:8080${publicPath}report.html`))
-      // console.log(chalk.green(`> Report at  http://localhost:${port}${publicPath}report.html`))
+      console.log(chalk.green(`> Report at  http://localhost:${port}${publicPath}report.html`))
     }
 
   })
