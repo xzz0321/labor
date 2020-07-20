ba7b8f04012015272137748838ede9baa973c9b0
杨贤贤 <13671115805@163.com>
2020-07-20 10:29:24 +0800

    静态资源存放位置

--- vue.config.js
+++ vue.config.js
@@ -21,7 +21,7 @@ module.exports = {
   // 在npm run build 或 yarn build 时 ，生成文件的目录名称（要和baseUrl的生产环境路径一致）（默认dist）
   outputDir: 'dist',
   // 用于放置生成的静态资源 (js、css、img、fonts) 的；（项目打包之后，静态资源会放在这个文件夹下）
-  assetsDir: 'static',
+  // assetsDir: 'static',
   // 是否开启eslint保存检测，有效值：ture | false | 'error'
   lintOnSave: process.env.NODE_ENV === 'development',
   // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建。
@@ -35,9 +35,9 @@ module.exports = {
       [process.env.VUE_APP_BASE_API]: {
         target: `http://localhost:80`,
         changeOrigin: true,
-        // pathRewrite: {
-        //   ['^' + process.env.VUE_APP_BASE_API]: ''
-        // }
+        pathRewrite: {
+          ['^' + process.env.VUE_APP_BASE_API]: ''
+        }
       }
     },
     disableHostCheck: true
