a3781bb2b511041cce5a32a8fb01eb6267570135
杨贤贤 <13671115805@163.com>
2020-07-23 14:03:40 +0800

    社保月报

---
+++ src/api/synthetical/monthly.js
@@ -0,0 +1,20 @@
+// 综合统计查询-社保月报查询
+import request from '@/utils/request'
+
+// 查询列表
+export function listMonthly (query) {
+  return request({
+    url: '',
+    method: 'get',
+    params: query
+  })
+}
+
+// 导出
+export function exportMonthly (query) {
+  return request({
+    url: '',
+    method: 'get',
+    params: query
+  })
+}
