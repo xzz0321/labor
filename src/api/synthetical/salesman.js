d0e28ee5745de20f43a7009e67407317c7e97049
杨贤贤 <13671115805@163.com>
2020-07-23 14:22:53 +0800

    业务员绩效明细

---
+++ src/api/synthetical/salesman.js
@@ -0,0 +1,20 @@
+// 综综合统计查询-业务员绩效明细列表
+import request from '@/utils/request'
+
+// 查询列表
+export function listSalesman (query) {
+  return request({
+    url: '',
+    method: 'get',
+    params: query
+  })
+}
+
+// 导出
+export function exportSalesman (query) {
+  return request({
+    url: '',
+    method: 'get',
+    params: query
+  })
+}
