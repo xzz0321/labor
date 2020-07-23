066e11d1e980c63e13a7b345d686c0e98c8822b4
杨贤贤 <13671115805@163.com>
2020-07-23 14:25:45 +0800

    社保增减员月报

---
+++ src/api/synthetical/socialSecurity.js
@@ -0,0 +1,20 @@
+// 综综合统计查询-社保增减员月报
+import request from '@/utils/request'
+
+// 查询列表
+export function listSocialSecurity (query) {
+  return request({
+    url: '',
+    method: 'get',
+    params: query
+  })
+}
+
+// 导出
+export function exportSocialSecurity (query) {
+  return request({
+    url: '',
+    method: 'get',
+    params: query
+  })
+}
