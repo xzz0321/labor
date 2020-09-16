f73c3e710d0dd5c827584c8296ba8d7ac36795e8
xzz0321 <1362032588@qq.com>
2020-09-16 10:43:04 +0800

    主体信息维护

---
+++ src/api/subject/company.js
@@ -0,0 +1,53 @@
+import request from '@/utils/request'
+
+// 查询用工单位（企业）列表
+export function listCompany(query) {
+  return request({
+    url: '/business/company/list',
+    method: 'get',
+    params: query
+  })
+}
+
+// 查询用工单位（企业）详细
+export function getCompany(id) {
+  return request({
+    url: '/business/company/' + id,
+    method: 'get'
+  })
+}
+
+// 新增用工单位（企业）
+export function addCompany(data) {
+  return request({
+    url: '/business/company',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改用工单位（企业）
+export function updateCompany(data) {
+  return request({
+    url: '/business/company',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除用工单位（企业）
+export function delCompany(id) {
+  return request({
+    url: '/business/company/' + id,
+    method: 'delete'
+  })
+}
+
+// 导出用工单位（企业）
+export function exportCompany(query) {
+  return request({
+    url: '/business/company/export',
+    method: 'get',
+    params: query
+  })
+}
